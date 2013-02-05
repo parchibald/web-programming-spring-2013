e118874f292808d85abaa7f4c98bfb12ea58aca4
parchibald <pxl@sonic.net>
2013-02-04 19:15:05 -0800

    initial

---
+++ lesson-02/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+ <head>
+  <title>parchibald.cs53.11b.home</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello Web. My name Is Paul Archibald, 
+ 	and I am studying web programming and GIS 
+ 	(geographical Information Systems) at SRJC.</p>'; ?> 
+ 
+ <form action="action.php" method="post">
+ <p>Your name: <input type="text" name="name" /></p>
+ <p>Your age: <input type="text" name="age" /></p>
+ <p><input type="submit" /></p>
+ <p> (this will take you to another page) </p>
+</form>
+ 
+
+
+ </body>
+</html>
